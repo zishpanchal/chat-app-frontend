c2ad7c63e2c15a41ec1d1010e4356f8b3b7c1843
Zishan Panchal <zpanchal2@gmail.com>
2024-01-29 01:02:59 +0530

    added tooltips

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -21,18 +21,27 @@ export default function ChatInput({handleSendMsg}) {
         }
   }
     return (
-    <div className='flex ms-2 justify-start items-center backdrop-blur border border-slate-300 rounded-full w-[54dvw]'>   
+    <div className='flex ms-2 justify-start items-center bg-white/50 border border-slate-300 rounded-full w-[54dvw]'>   
     
     <form className='flex' onSubmit={(e)=>{sendChat(e)}}>
         <div>
         <input placeholder='Type your message here' value={msg} onChange={(e)=>{setMsg(e.target.value)}} type='text' className='text-md placeholder:italic placeholder:text-slate-400 block bg-transparent w-[48dvw] pr-3 rounded-full py-2 pl-9 shadow-sm focus:outline-none focus:border-orange-500 focus:ring-orange-500 focus:ring-1' />
         </div>
         <div className='flex items-center'>
-            <div onClick={handleShowEmojiPicker} className='ps-3 cursor-pointer'><span title='Emojis' className='text-2xl subpixel-antialiased'>😊</span></div>
+          <div className='group'>
+            <div onClick={handleShowEmojiPicker} className='ps-3 cursor-pointer'><span className='text-2xl subpixel-antialiased'>😊</span></div>
+            <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible absolute bottom-10 bg-orange-700 text-white text-xs p-2 rounded mt-2 transition-opacity">
+            <p className='w-max'> Emojis</p>
+            </div>
+            </div>
             {showEmojiPicker && <Picker width={290} skinTonePickerLocation='PREVIEW' className='bg-orange-200 top-[-455px] right-1' style={{position: "absolute"}} onEmojiClick={handleEmojiClick}/>}
-        <button type='submit' className='ps-2'><IoChevronForwardOutline title='Click to send' className='size-6 subpixel-antialiased' /></button>
+        <div className='flex group'>
+        <button type='submit' className='ps-2'><IoChevronForwardOutline className='size-6 subpixel-antialiased' /></button>
+        <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible absolute bottom-10 bg-orange-700 text-white text-xs p-2 rounded mt-2 transition-opacity">
+            <p className='w-max'>Click to send</p>
+            </div>
+            </div>
         </div>
-        
     </form>
     </div>
   )
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -10,8 +10,11 @@ export default function Logout() {
   }
   
     return (
-    <div onClick={handleClick} className="cursor-pointer  p-1 rounded-full" title="Click to logout!" >
-        <IoLogOutOutline title="Click to logout!" className='h-5 w-5 font-open'/>
+    <div onClick={handleClick} className=" cursor-pointer group p-1 rounded-full">
+        <IoLogOutOutline className='h-5 w-5 font-open'/>
+        <div className="opacity-0 z-10 invisible group-hover:opacity-100 group-hover:visible absolute bg-orange-700 text-white text-xs p-2 rounded mt-2 transition-opacity">
+       <p className='w-max'> Click to logout!</p>
+      </div>
     </div>
   )
 }
